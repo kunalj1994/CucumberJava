c206fb0f0b54e2511e12eb900b82a6a4658709ff
Kunal <kunalj1994@gmail.com>
2018-06-06 16:59:38 -0400

    firstcommit

---
+++ src/test/java/Sample/Myfirstfeaturefiletest.java
@@ -0,0 +1,14 @@
+package Sample;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions()
+
+public class Myfirstfeaturefiletest {
+	
+
+}
---
+++ src/test/java/Sample/Myfirststepdef.java
@@ -0,0 +1,36 @@
+package Sample;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+
+public class Myfirststepdef {
+	
+	WebDriver driver;
+	
+	@Given("^User is at facebook login page$")
+	public void User_is_at_facebook_login_page(){
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Jharna\\Desktop\\Cucumber Setup\\chromedriver_win32\\chromedriver.exe");
+		driver  = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		}
+
+	@When ("^User enters user first name$")
+	public void User_enters_user_first_name() {
+		driver.findElement(By.xpath("//*[@id=\"u_0_9\"]")).sendKeys("Jharna");
+			}
+	
+	@Then ("^User checks user first name is present$") 
+		public void User_checks_user_first_name_is_present() {
+		String userNameActual = driver.findElement(By.xpath("//*[@id=\"u_0_9\"]")).getAttribute("value");
+		Assert.assertEquals("David", userNameActual);
+	}
+	
+	
+}
